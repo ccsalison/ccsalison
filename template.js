9fcec305d62bd962eca0fbc2c48568856d2c8a69
Alison Chan <21253748+ccsalison@users.noreply.github.com>
2019-05-24 19:34:42 -0500

    Update template.js

--- template.js
+++ template.js
@@ -20,3 +20,9 @@ var containsDuplicate = function(nums) {
     if (nums.length == myset.size){return false}
     else return true
 };
+
+var months = ['Jan', 'March', 'April', 'June'];
+months.splice(4, 1, 'May');
+// replaces 1 element at index 4
+console.log(months);
+// expected output: Array ['Jan', 'Feb', 'March', 'April', 'May']
