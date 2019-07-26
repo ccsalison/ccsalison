6e5fde94acc90096dfe24105967b6c3eb0dfad45
Alison Chan <21253748+ccsalison@users.noreply.github.com>
2019-07-25 19:14:41 -0500

    Added cycle detection function.

--- DataStructure/Graph.js
+++ DataStructure/Graph.js
@@ -1,3 +1,5 @@
+// Tutorial from : https://hackernoon.com/the-javascript-developers-guide-to-graphs-and-detecting-cycles-in-them-96f4f619d563
+
 function Graph(){
   this.adjList = {}
 }
@@ -34,13 +36,49 @@ Graph.prototype._dfsUtil = function(vertex,visited) {
   }
 }
 
+Graph.prototype.detectCycle = function() {
+  const graphNodes = Object.keys(this.adjList);
+  const visited = {};
+  const recStack = {};
 
-//Testing
+  for(let i = 0; i < graphNodes.length;i++){
+    const node = graphNodes[i]
+    if(this._detectCycleUtil(node,visited,recStack))
+    return 'there is a cycle'
+  }
+  return 'no cycle!'
+}
+
+Graph.prototype._detectCycleUtil = function(vertex, visited, recStack){
+  if(!visited[vertex]){
+    visited[vertex] = true
+    recStack[vertex] = true
+    const nodeNeighbors = this.adjList[vertex]
+    for(let i = 0; i<nodeNeighbors.length;i++){
+      const currentNode = nodeNeighbors[i]
+      console.log('parent',vertex,'Child', currentNode)
+      if(!visited[currentNode] && this._detectCycleUtil(currentNode,visited, recStack)){
+        return true;
+      }else if(recStack[currentNode]){
+        return true;
+      }
+    }
+  }
+  recStack[vertex] = false
+  return false
+}
+
+
+
+// Testing
 const graph = new Graph()
 
 graph.addVertex('A')
 graph.addVertex('B')
-
 graph.addEdge('A','B')
 
+console.log(graph.detectCycle())
 console.log(graph)
+
+graph.addEdge('B','A')
+console.log(graph.detectCycle())
